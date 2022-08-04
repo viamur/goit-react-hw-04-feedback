c0b05c6dba64a9def45ba9d180de41664e1bbb61
Moskalenko Serhii <mynameserhii@gmail.com>
2022-08-04 20:43:17 +0300

    move options top

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,12 +4,13 @@ import Statistics from './Statistics/Statistics';
 import FeedbackOptions from './FeedbackOptions/FeedbackOptions';
 import Notification from './Notification/Notification';
 
+const options = ['good', 'neutral', 'bad'];
+
 export const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
-  const options = ['good', 'neutral', 'bad'];
   const total = good + neutral + bad;
   const positivePercentage = total ? Math.ceil((good / total) * 100) : 0;
 
